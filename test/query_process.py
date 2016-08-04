5417619b3ef1ea3b015e1ceb4de9e1e0495f7714
Guanyu Wang <guanyuwang.cs@gmail.com>
2016-08-04 15:16:16 -0400

    Update query processing

--- test/query_process.py
+++ test/query_process.py
@@ -61,34 +61,51 @@ for k,v in queries.iteritems():
 
 avg_q_len = float(avg_q_len)/64.0  
 
-#print query_terms
-def process_query(query_terms, algo):
-  q_len = len(query_terms)
-  for a_term in query_terms:
-    count = terms_count.get(a_term)
-    if count == None:
-      count = 0
-    terms_count[a_term]=(count + 1)
+def output_query(q_id, scores, algo_name, fw):
+  rank = 1
+  for score in scores:
+    if rank > 1000: break
+    line = "%d Q0 CACM-%s %d %f %s\n" % (q_id, score[0], rank, score[1], algo_name)
+    rank += 1
+    fw.write(line)
 
-  db = DB_Session()
+def process_single_query(q_id, db, query_terms, algo, fw):
+  q_len = len(query_terms)
   q = newQueryObj(query_terms)
   query_info = db.getDocsFromQuery(q, 3204)
-  if algo == 'okapi':
-    return query_info.getOkapiScores(False, 3204, 1, 35, avg_q_len)
-  elif algo == 'okapi_idf':
-    return query_info.getOkapiScores(True, 3204, 1, 35, avg_q_len)
-  elif algo == 'laplace':
-    return query_info.getLaplaceSmoothingScores(9742)
-  return []
+  scores = None
+  if algo == 'Okapi':
+    scores = query_info.getOkapiScores(False, 3204, 1, 35, avg_q_len)
+  elif algo == 'Okapi_IDF':
+    scores = query_info.getOkapiScores(True, 3204, 1, 35, avg_q_len)
+  elif algo == 'Laplace':
+    scores = query_info.getLaplaceSmoothingScore(9742)
+  output_query(q_id, scores, algo, fw)
 
+def process_query(q_id, query_terms, algo, f_name):
+  fw = open(f_name, "w")
+  db = DB_Session()
+  if q_id >= 1 and q_id <= 64:
+    q_terms = queries[q_id]
+    process_single_query(q_id, db, q_terms, algo, fw)
+  elif q_id == 0:
+    for q_idx in range(1, 65):
+      q_terms = queries[q_idx]
+      process_single_query(q_idx, db, q_terms, algo, fw)
+  else:
+    process_single_query(-1, db, query_terms, algo, fw)
+  fw.close()
+    
 #print q_id
+if len(sys.argv) < 4:
+  print "Usage: %s <query id> <algorithm> <output file>"
+  sys.exit(1)
 q_id = int(sys.argv[1])
 algo = sys.argv[2]
-if q_id >=1 and q_id <= 64:
-  query_terms = queries.get(q_id)
-
-else:
+f_name = sys.argv[3]
+query_terms = None
+if q_id == -1:
   input_query = raw_input("Please enter your query terms, press ENTER to finish:")
   query_terms = input_query.split()
   query_terms = process(query_terms, stop_words)
-print process_query(query_terms, algo)
+process_query(q_id, query_terms, algo, f_name)
